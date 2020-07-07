cc5e886871e322073b7320d4a49e7949ca3e2661
kjkandrea <32591477+kjkandrea@users.noreply.github.com>
2020-07-07 13:11:54 +0900

    mongodb 연결

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const masterKey = require('./password')
+
+const mongoose = require('mongoose')
+mongoose.connect(masterKey.MongoDB.code, {
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+})
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err))
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
