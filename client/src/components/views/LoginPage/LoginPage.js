4afa4015007cb285a70f5b3392865193e7a2fc0d
kjkandrea <32591477+kjkandrea@users.noreply.github.com>
2020-07-08 18:10:45 +0900

    client : 로그인 요청 redux를 통해 구현

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { loginUser } from '../../../_actions/user_action'
 
-function LoginPage() {
+function LoginPage(props) {
+  const dispatch = useDispatch()
 
   const [Email, setEmail] = useState("")
   const [Password, setPassword] = useState("")
@@ -13,10 +16,29 @@ function LoginPage() {
     setPassword(event.currentTarget.value)
   }
 
+  const onSubmitHander = (event) => {
+    event.preventDefault()
+
+    let body = {
+      email: Email,
+      password: Password
+    }
+
+    dispatch(loginUser(body))
+      .then(response => {
+        if(response.payload.loginSuccess) {
+          props.history.push('/')
+        } else {
+          alert('Error')
+        }
+      })
+  }
+
   return (
     <div style={{ display:'flex', justifyContent:'center', alignItems:'center', width:'100%', height: '100vh'}}>
       <form
         style={{ display:'flex', flexDirection:'column'}}
+        onSubmit={onSubmitHander}
       >
         <label>Email</label>
         <input type="email" value={Email} onChange={onEmailHandler} />
