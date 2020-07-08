ad8b9b25d57c4f6e573a43c0316ca5133d9f0291
kjkandrea <32591477+kjkandrea@users.noreply.github.com>
2020-07-08 19:10:18 +0900

    client : 로그아웃 구현

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,14 +1,29 @@
 import React, { useEffect } from 'react'
 import axios from 'axios'
 
-function LandingPage() {
+function LandingPage(props) {
   useEffect(() => {
     axios.get('/api/hello').then((response) => {console.log(response.data)})
   }, [])
 
+  const onClickHandler = () => {
+    axios.get('/api/users/logout')
+      .then(response => {
+        if(response.data.success) {
+          props.history.push("/login")
+        }else {
+          alert("logout fail")
+        }
+      })
+  }
+
   return (
-    <div style={{ display:'flex', justifyContent:'center', alignItems:'center', width:'100%', height: '100vh'}}>
+    <div
+      style={{ display:'flex', justifyContent:'center', alignItems:'center', width:'100%', height: '100vh'}}
+    >
       <h2>시작 페이지</h2>
+
+      <button onClick={onClickHandler}>로그아웃</button>
     </div>
   )
 }
