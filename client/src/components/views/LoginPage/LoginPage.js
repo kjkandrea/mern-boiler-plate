add048c993bce1cdc07fb86370d5b2cfb079e9e2
kjkandrea <32591477+kjkandrea@users.noreply.github.com>
2020-07-08 17:25:12 +0900

    client : 로그인 폼 컴포넌트 레이아웃 생성

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function LoginPage() {
+
+  const [Email, setEmail] = useState("")
+  const [Password, setPassword] = useState("")
+
+  const onEmailHandler = (event) => {
+    setEmail(event.currentTarget.value)
+  }
+
+  const onPasswordHandler = (event) => {
+    setPassword(event.currentTarget.value)
+  }
+
   return (
-    <div>
-      LoginPage
+    <div style={{ display:'flex', justifyContent:'center', alignItems:'center', width:'100%', height: '100vh'}}>
+      <form
+        style={{ display:'flex', flexDirection:'column'}}
+      >
+        <label>Email</label>
+        <input type="email" value={Email} onChange={onEmailHandler} />
+        <label>Password</label>
+        <input type="password" value={Password} onChange={onPasswordHandler} />
+        <button type="submit">Login</button>
+      </form>
     </div>
   )
 }
--- client/src/index.js
+++ client/src/index.js
@@ -5,17 +5,20 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from "react-redux";
 import 'antd/dist/antd.css';
+import { createStore, applyMiddleware } from 'redux';
 import promiseMiddleware from 'redux-promise';
-import ReduxThunk from 'redux-thunk'
-import Reducer from './_reducers'
+import ReduxThunk from 'redux-thunk';
+import Reducer from './_reducers';
 
-const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore)
 
 ReactDOM.render(
   <Provider
-    store={createStoreWithMiddleware(Reducer,
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-      )}
+    store={createStoreWithMiddleware(
+      Reducer,
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__()
+    )}
   >
     <App />
   </Provider>,
